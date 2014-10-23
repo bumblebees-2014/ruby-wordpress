eababc36b5e672a970ef4590d420c6112b3e670f
steveono <steveono@gmail.com>
2014-10-23 01:51:16 -0400

    added a little more ajax

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,3 +3,15 @@ get '/' do
 	@articles
 	erb :index
 end
+
+post '/' do
+	puts "--------------------------------------------------------->"
+	@json_articles = Article.all.limit(4).offset(params[:count]).to_json
+
+ #  @json_articles.to_a.map!(&:serializable_hash)
+	# @json_articles.each do |message|
+	# 	message["name"] = User.find_by(id: message["sender_id"]).name
+	# end
+	# p @json_articles.to_json
+	@json_articles
+end
--- public/js/application.js
+++ public/js/application.js
@@ -17,4 +17,111 @@ $(document).ready(function() {
 			$('.comment_list').prepend(response);
 		});
 	});
-});
+
+	var count = 12
+
+  $('#load_more').on("click", function(event) {
+    event.preventDefault()
+    console.log(count)
+
+    $.ajax({
+      type: "POST",
+      url: '/',
+      data: {"count": count}
+    })
+    .done(function(response) {
+      count += 4;
+      messages = jQuery.parseJSON(response)
+
+
+      for (var i = 0; i < messages.length; i++) {
+        $('#page_bottom').append(response)
+      }
+      console.log(messages)
+    })
+  })
+
+
+	// setTimeout(function(){
+	// 	$(window).scroll(function () {
+	// 		if ($(document).height() <= $(window).scrollTop() + $(window).height()) {
+	// 			getArticles()
+	// 		}
+	// 	})
+	// },500);
+
+ //  var count = 12
+
+ //  var getArticles = function() {
+ //    console.log(count)
+
+ //    $.ajax({
+ //      type: "POST",
+ //      url: '/',
+ //      data: {"count": count}
+ //    })
+ //    .done(function(response) {
+ //      count += 4;
+ //      messages = jQuery.parseJSON(response)
+
+
+ //      for (var i = 0; i < messages.length; i++) {
+ //        $('#page_bottom').append(response)
+ //      }
+ //      console.log(messages)
+ //    })
+ //  }
+})
+
+
+
+
+
+
+
+
+
+// $(document).ready(function() {
+//   var startLoad = 51
+//       setTimeout(function(){
+//   $(window).scroll(function () {
+//     if ($(document).height() <= $(window).scrollTop() + $(window).height()) {
+//          getMessages()
+//     }
+//   })
+// },500);
+
+// var getMessages = function(){
+//   $.ajax({
+//     type: "GET",
+//     url: "/inbox/"+ startLoad+""
+//   }).done(function(response){
+//       console.log("BAM")
+//       messages = jQuery.parseJSON(response)
+//       for (var i = 0; i < messages.length; i++){
+//         // console.log(response)
+//         console.log(messages)
+//         // $('#inbox_table').append(response)
+//         // $('#inbox_table').append("<li 'class=ajax"+[i]+"'>"+ (startLoad + i) + ". " + "<a href='/messages/" + messages[i].id + "'>" + messages[i].subject+"</a></li>")
+//       }
+//       startLoad += 10
+//   })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
